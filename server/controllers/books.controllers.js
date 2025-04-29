f8c3c0df29566ac9505bf5ad3c915a3f6d383687
Nico Ventura <nicolas.ventura@thebridgeschool.es>
2025-04-29 12:06:46 +0200

    Change dependencies

--- server/controllers/books.controllers.js
+++ server/controllers/books.controllers.js
@@ -1,11 +1,11 @@
-const db = require("../db")
+const Book = require("../models/Book")
 
 const getBooks = (req, res) => {
     res.send("Test")
 }
 
 const createBook = async (req, res) => {
-    const createdBook = await db.books.create({title: "Cinco semanas en globo", description: "Hola", length: 350})
+    const createdBook = await Book.create({title: "Cinco semanas en globo", description: "Hola", length: 350})
     res.send(createdBook.id)
 }
 
